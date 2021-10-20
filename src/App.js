a3cf7f3afd3621429d6a79646e52493428b96704
pablolucianop <pablolucianop@gmail.com>
2021-10-20 12:11:42 -0300

    approach stage

---
+++ src/App.js
@@ -0,0 +1,155 @@
+import logo from './logo.svg'
+import './App.css'
+import React from 'react'
+import Badge from 'react-bootstrap/Badge'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+import ListGroup from 'react-bootstrap/ListGroup'
+import axios from 'axios'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+
+class Prue extends React.Component {
+  render() {
+    // console.log('this.props.este.term', this.props.este.term)
+    return (
+      <Card style={{ width: '18rem' }}>
+        <Card.Body>
+          <Card.Title>{this.props.este}</Card.Title>
+          <Button variant="primary">🡅</Button>{' '}
+          <Button variant="secondary">🡇</Button>
+          {/* <p>{this.props.este.term}</p> */}
+        </Card.Body>
+      </Card>
+    )
+  }
+}
+
+class TodosS extends React.Component {
+  render() {
+    console.log('this.props.todos', this.props.todos)
+    let navLinks = <Prue este={'pers'} />
+    if (this.props.todos !== undefined) {
+
+let unicos = [...new Set(this.props.todos.map((este) => este.sin))]
+console.log('unicos', unicos)
+
+       navLinks = unicos.map((pers) => {
+         return <Prue este={pers} />
+       })
+    }
+
+    return navLinks
+  }
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      allSyns: '',
+    }
+  }
+
+  componentDidMount() {
+    // axios
+    //   .get(
+    //     'https://www.abbreviations.com/services/v2/syno.php?uid=9413&tokenid=vIMVCwch6JUkn04H&word=consistent&format=json'
+    //   )
+    //   .then((response) => {
+    //     let syns = response.data.result.map((x) => (x = x.term))
+    //   console.log(response.data.result)
+    //     console.log(syns)
+    //     this.setState({ allSyns: syns })
+    //   })
+    //   .catch((error) => {
+    //     console.log(error)
+    //   })
+    let mainWord = 'consistent'
+    let responsen = [
+      {
+        antonyms:
+          'at odds(p), scratchy, unreconciled, uneven, contradictory, inconsistent, conflicting, incompatible, spotty, heterogeneous, discrepant, heterogenous, self-contradictory, unconformable',
+        definition:
+          "(sometimes followed by `with') in agreement or consistent or reliable",
+        example:
+          '"testimony consistent with the known facts"; "I have decided that the course of conduct which I am following is consistent with my sense of responsibility as president in time of war"- FDR',
+        partofspeech: 'adj',
+        synonyms: 'ordered, coherent, logical, reproducible, uniform',
+        term: 'consistent',
+      },
+
+      {
+        antonyms:
+          'scratchy, spotty, contradictory, conflicting, incompatible, uneven, heterogenous, inconsistent, unreconciled, heterogeneous, at odds(p), self-contradictory, unconformable, discrepant',
+        definition: 'capable of being reproduced',
+        example: '"astonishingly reproducible results can be obtained"',
+        partofspeech: 'adj',
+        synonyms:
+          'uniform, ordered, coherent, logical, reproducible, consistent',
+        term: 'reproducible, consistent',
+      },
+
+      {
+        antonyms:
+          'discrepant, heterogenous, contradictory, inconsistent, conflicting, scratchy, uneven, at odds(p), unconformable, heterogeneous, self-contradictory, incompatible, spotty, unreconciled',
+        definition:
+          'marked by an orderly, logical, and aesthetically consistent relation of parts',
+        example: '"a coherent argument"',
+        partofspeech: 'adj',
+        synonyms:
+          'coherent, consistent, ordered, uniform, tenacious, lucid, logical, legitimate, arranged, reproducible',
+        term: 'coherent, consistent, logical, ordered',
+      },
+
+      {
+        antonyms:
+          'unreconciled, inconsistent, at odds(p), discrepant, incompatible, unconformable, heterogenous, heterogeneous, uneven, contradictory, conflicting, spotty, scratchy, self-contradictory',
+        definition: 'the same throughout in structure or composition',
+        example:
+          '"bituminous coal is often treated as a consistent and homogeneous product"',
+        partofspeech: 'adj',
+        synonyms:
+          'coherent, consistent, ordered, uniform, unvarying, logical, undifferentiated, reproducible',
+        term: 'consistent, uniform',
+      },
+    ]
+    let syns = responsen[0].synonyms.split(',')
+    // let synsMapp = responsen.map((x) => [x.synonyms])
+    let synsMapp = responsen.map(
+      (x) => (x = { term: x.term.split(','), syn: x.synonyms.split(',') })
+    )
+
+    console.log('synsMapp', synsMapp)
+    let simples = []
+    synsMapp.forEach((element) => {
+      element.syn.forEach((element0) => {
+        simples.push({
+          sin: element0,
+          term: element.term.filter(
+            (word) => word === word // !== mainWord
+          ),
+          main: mainWord,
+        })
+      })
+    })
+
+    let simpsObs = { todos: simples }
+    this.setState({ allSyns: simpsObs })
+  }
+
+  render() {
+    const { allSyns } = this.state
+    return (
+      <div>
+        <h1>Hola</h1>
+        {console.log(232, this.state.allSyns.todos)}
+
+        <TodosS todos={this.state.allSyns.todos} />
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/App2.js
@@ -0,0 +1,165 @@
+import logo from './logo.svg'
+import './App.css'
+import React from 'react'
+import Badge from 'react-bootstrap/Badge'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+import ListGroup from 'react-bootstrap/ListGroup'
+import axios from 'axios'
+
+// import Jumbotron from 'react-bootstrap/Jumbotron'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function getPokemonInfo() {
+axios.get('https://api.agify.io/?name=Brayan`', {
+ headers: {
+   Authorization: '563492ad6f917000010000018a9e9260684740c78c441ff6264931e5'
+ }
+})
+
+  axios
+    .get(`https://api.agify.io/?name=Brayan`),
+    // .get(`https://pokeapi.co/api/v2/pokemon/${stateSearch}/`)
+    .then((response) => {
+      // alert(response)
+      console.log(response.data.age)
+    })
+    .catch((error) => {
+      // alert(`${stateSearch} is not a pokemon`)
+    })
+}
+getPokemonInfo()
+// alert('response')
+class Colectivo extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { texto: 1 }
+    this.changeColor = this.changeColor.bind(this)
+  }
+  changeColor() {
+    getPokemonInfo()
+    const newColor = ++this.state.texto
+    this.setState({ texto: newColor })
+  }
+
+  render() {
+    const univ = this.props.univ
+    // alert(this.props.univ.gente)
+    const navLinks = univ.gente.map((pers) => {
+      return <Persona perso={pers} univ={univ} />
+    })
+    let navLinksResu = navLinks.splice(0, this.state.texto)
+
+    return (
+      <nav onClick={this.changeColor}>
+        {this.state.texto}
+        {navLinksResu}
+      </nav>
+    )
+  }
+}
+
+class Persona extends React.Component {
+  render() {
+    // console.log('this.props.univ', this.props.univ.organos)
+    let univo = this.props.univ
+    let saluu = this.props.perso.salud
+    // console.log('univo', univo)
+    let oferta = (
+      <h3>
+        <Badge bg="secondary">OFERTA</Badge>
+      </h3>
+    )
+    let oferta2 = <h3></h3>
+    function sayHello() {
+      console.log('You clicked me!' + saluu)
+    }
+    return (
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={this.props.perso.foto} />
+        <Card.Body>
+          {saluu < 5 ? oferta : oferta2}
+          <Card.Title>
+            {' '}
+            {this.props.perso.firstName} {this.props.perso.lastName}
+          </Card.Title>
+          <Card.Text>Salud {this.props.perso.salud}</Card.Text>
+          <SacoDeOrganos univo={univo} sal={saluu} />
+          <Button variant="primary">Primary</Button>{' '}
+        </Card.Body>
+      </Card>
+    )
+  }
+}
+
+class SacoDeOrganos extends React.Component {
+  render() {
+    function getRandomInt(max) {
+      return Math.floor(Math.random() * max)
+    }
+    const sal = this.props.sal
+    let oo = this.props.univo.organos.slice(0, sal)
+    const navLinks = oo.map((org, i) => {
+      return (
+        <ListGroup.Item>
+          {org} {getRandomInt(10) * this.props.sal}%
+        </ListGroup.Item>
+      )
+    })
+    return (
+      <nav>
+        <ListGroup>{navLinks} </ListGroup>{' '}
+      </nav>
+    )
+  }
+}
+
+function doIT(params) {
+  console.log('You clicked me!' + params)
+  // alert(999)
+}
+
+function Person(first, last, age, salud, foto) {
+  this.firstName = first
+  this.lastName = last
+  this.salud = salud
+  this.foto = foto
+  this.func = doIT
+}
+
+let marioFot = 'https://i.imgur.com/mtbl1cr.jpeg'
+let normanFot = 'https://i.imgur.com/vpKNYfK.jpeg'
+let canarFot = 'https://i.imgur.com/YY9iR9G.png'
+
+const marioPereyra = new Person('Mario', 'Pereyra', 56, 10, marioFot)
+const normanBrisky = new Person('Norman', 'brisky', 130, 1, normanFot)
+const canarioLopez = new Person('Canario', 'Lopez', 16, 5, canarFot)
+
+function App() {
+  let univ = {
+    nombres: ['Juan Carlos II', 'Elda', 'Elga', 'Tito', 'art', 'shop'],
+
+    organos: [
+      'pancreas',
+      'celebro',
+      'ojos',
+      'estómago',
+      'esternon',
+      'pulmones',
+      'órgano alma',
+    ],
+    gente: [marioPereyra, normanBrisky, canarioLopez],
+  }
+
+  // Person('Mario', 'Pereyra', 100)
+
+  return (
+    <div className="App">
+      {/* <Jumbotron>rrrr </Jumbotron> */}
+      <Colectivo univ={univ} />
+    </div>
+  )
+}
+
+export default App
