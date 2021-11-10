c4b602257d3ec890feed9d83f05ff69c225ec4bb
pablolucianop <pablolucianop@gmail.com>
2021-11-10 20:52:44 -0300

    wordButton F

--- src/WordButton.js
+++ src/WordButton.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react';
 import Badge from 'react-bootstrap/Badge'
 import Card from 'react-bootstrap/Card'
 import { useSpring, animated } from 'react-spring'
@@ -14,35 +14,66 @@ function Appo() {
   return <animated.div   style={props}>  hhh     </animated.div>
 }
 
-export class WordButton extends React.Component {
-  constructor() {
-    super();
-    this.submitHandler2 = this.submitHandler2.bind(this);
-    this.submitHandler22 = this.submitHandler22.bind(this);
-    this.state = {
+// export class WordButton extends React.Component {
+//   constructor() {
+//     super();
+//     this.submitHandler2 = this.submitHandler2.bind(this);
+//     this.submitHandler22 = this.submitHandler22.bind(this);
+//     this.state = {
+//       picked2: '',
+//       unPicked2: '',
+//     };
+//   }
+//   submitHandler2(evt) {
+//     evt.preventDefault();
+//     this.props.submitHandler([this.props.synCard])
+//   }
+//   submitHandler22(evt) {
+//     evt.preventDefault();
+//     this.props.eee(this.props.synCard)
+//   }
+
+//   render() {
+    
+//     return (
+//       <div className="p-2">
+//         <Card type="button" className="btn btn-light"  >
+//           <Appo > hhe</Appo>
+//           <h5 ><div onClick={this.submitHandler2}>{this.props.synCard}</div><div>{(this.props.eee !== undefined) ? <span className="badge text-muted" onClick={this.submitHandler22}>╳</span> : ''}</div></h5> 
+        
+//                  </Card>
+//       </div>
+//     )
+//   }
+// }
+
+
+export function WordButton (props)  {
+
+  const [wBState, setwBState] = useState(
+    {
       picked2: '',
       unPicked2: '',
-    };
-  }
-  submitHandler2(evt) {
+    }
+  );
+
+  const submitHandler2 = (evt) =>{
     evt.preventDefault();
-    this.props.submitHandler([this.props.synCard])
-  }
-  submitHandler22(evt) {
-    evt.preventDefault();
-    this.props.eee(this.props.synCard)
+    props.submitHandler([props.synCard])
   }
 
-  render() {
-    
+  const submitHandler22 =(evt) => {
+    evt.preventDefault();
+    props.eee(props.synCard)
+  }
+
     return (
       <div className="p-2">
         <Card type="button" className="btn btn-light"  >
           <Appo > hhe</Appo>
-          <h5 ><div onClick={this.submitHandler2}>{this.props.synCard}</div><div>{(this.props.eee !== undefined) ? <span className="badge text-muted" onClick={this.submitHandler22}>╳</span> : ''}</div></h5> 
+          <h5 ><div onClick={submitHandler2}>{props.synCard}</div><div>{(props.eee !== undefined) ? <span className="badge text-muted" onClick={submitHandler22}>╳</span> : ''}</div></h5> 
         
                  </Card>
       </div>
     )
-  }
 }
